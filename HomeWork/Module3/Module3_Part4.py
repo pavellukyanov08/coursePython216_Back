df8e5b406f4e36b3d5d4bed76f86c92dfacf839e
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-01 16:39:44 +0700

    Added: task2 module3_part4 is completed

---
+++ HomeWork/Module3/Module3_Part4.py
@@ -0,0 +1,19 @@
+# Task 1
+# nums1 = int(input('Введите первую границу: '))
+# nums2 = int(input('Введите вторую границу: '))
+#
+# k = 0
+# for i in range(nums1, nums2 // nums1 + 1):
+#     if nums1 % i == 0:
+#         k = k+1
+# if k <= 0:
+#     print("Число простое")
+# else:
+#     print("Число не является простым")
+
+
+# Task 2
+# for i in range(1, 10):
+#     for j in range(1, 10):
+#         print(f'{i} * {j} = {i * j}', end='\t')
+#     print()
