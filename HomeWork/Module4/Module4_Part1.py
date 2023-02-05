fbe583b380edcf37e33214e2f8ab6554fa92f321
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-05 18:34:24 +0700

    Adited: task1 module4_part1 is completed

---
+++ HomeWork/Module4/Module4_Part1.py
@@ -0,0 +1,12 @@
+# Task 1
+# string = input('Введите строку: ')
+#
+# string = string.lower()
+#
+# forbidden = ('.', '?', '!', ':', ';', '-', '—', ' ', ',')
+# text = ''.join(symbol for symbol in string if symbol not in forbidden)
+#
+# if text == text[::-1]:
+#     print('Слово палиндром')
+# else:
+#     print('Слово не палиндром')
