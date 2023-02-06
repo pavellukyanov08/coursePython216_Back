6d3526af3de077aa07f7271878121af0069f6ed0
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-06 21:59:34 +0700

    Adited: task2 module4_part1 is completed

--- HomeWork/Module4/Module4_Part1.py
+++ HomeWork/Module4/Module4_Part1.py
@@ -10,3 +10,20 @@
 #     print('Слово палиндром')
 # else:
 #     print('Слово не палиндром')
+
+
+# Task 2
+# text = input('Введите текст: ')
+# words = input('Введите слова: ')
+#
+# for words in words.split():
+#     text = text.replace(words, words.upper())
+# print(f'Измененный текст: {text}')
+
+
+# Task 3
+# text = 'Lorem ipsum dolor. Sit amet consectetur. Adipisicing elit. Similique, tempore.'
+
+# for i in range(len(text)):
+#     text = '.'.join()
+#     print(i)
