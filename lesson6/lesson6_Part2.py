f5397e10cadb7071a280c5ad93b401c1ed116f51
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-16 14:04:51 +0700

    Adited file

--- lesson6/lesson6_Part2.py
+++ lesson6/lesson6_Part2.py
@@ -1,51 +1,41 @@
-import random
-
 # n = int(input('Введите ширину матрицы: '))
 # m = int(input('Введите высоту матрицы: '))
 # row = [0] * n
 # matrix = [row] * m
 # for row in matrix:
 #     print(*row)
-# print(matrix[0][2])
 
-
+# import random
 # n = int(input('Введите ширину матрицы: '))
 # m = int(input('Введите высоту матрицы: '))
 # matrix = [[random.randint(1, 10) for i in range(n)] for j in range(m)]
 #
 # for row in matrix:
-#     for elm in row:
-#         print(elm, end='\t')
+#     for num in row:
+#         print(num, end='\t')
 #     print()
 
 
 # heights = ['167', '175', '171', '180']
-# heights = list(map(int, heights))
+# another_list = []
+# heights = list(map(str.split, heights))
 # print(heights)
+# print(another_list)
 
-
-# nums = list(map(int, input('Введите значения через пробел: ').split()))
-#
-# print(type(nums))
+# nums = list(map(int, input('Введите сколько угодно '
+#                            'чисел через пробел: ').split()))
 # print(nums)
 
 
-# n = int(input('Введите ширину матрицы: '))
-# m = int(input('Введите высоту матрицы: '))
-# matrix = [[i for i in range(n)] for j in range(m)]
-#
-# for row in matrix:
-#     for elm in row:
-#         print(elm, end='\t')
-#     print()
-
-
+# 1.
 # matrix = []
 # n = int(input('Введите ширину матрицы: '))
 # m = int(input('Введите высоту матрицы: '))
-# # matrix = [[i + n * j + 1 for i in range(n)] for j in range(m)]
+
+# matrix = [[i + n * j + 1 for i in range(n)] for j in range(m)]
+#  ^^^ Генерация матрицы в одну строку через генераторы ^^^
+
 # value = 1
-#
 # for i in range(m):
 #     row = []
 #     for j in range(n):
@@ -55,39 +45,14 @@ import random
 #     print()
 #     matrix.append(row)
 # print('\t' * m)
-#
 # for i in range(m):
 #     for j in range(n):
 #         matrix[i][j] **= 2
-#         print(matrix[i][j] **= 2
+#         print(matrix[i][j], end='\t')
 #     print()
 
 
-# matrix = []
-# n = int(input('Введите ширину матрицы: '))
-# m = int(input('Введите высоту матрицы: '))
-# # matrix = [[i + n * j + 1 for i in range(n)] for j in range(m)]
-# value = 1
-#
-# for i in range(m):
-#     row = []
-#     for j in range(n):
-#         row.append(value)
-#         print(value, end='\t')
-#         value += 1
-#     print()
-#     matrix.append(row)
-# print('\t' * m)
-#
-# for i in range(m):
-#     for j in range(n):
-#         matrix[i][j] **= 2
-#         print(matrix[i][j] **= 2
-#     print()
-
-
-# wide = 10
-# height = 10
+# 2.
 # matrix = [[0 for i in range(10)] for j in range(10)]
 # for i in range(10):
 #     matrix[i][i] = 1
@@ -96,15 +61,66 @@ import random
 #     if i > 0:
 #         matrix[i][i - 1] = 1
 #
-# for num in matrix:
-#     for j in row:
+# for row in matrix:
+#     for num in row:
 #         print(num, end='\t')
 #     print()
 
 
+# 3.
+# import random
+# n, m = 5, 5
+# matrix = [[random.randint(1, 20) for i in range(n)] for j in range(m)]
+#
+# for row in matrix:
+#     for num in row:
+#         print(num, end='\t')
+#     print()
+#
+# maximum = matrix[0][0]
+# for row in matrix:
+#     maximum = max(maximum, max(row))
+#
+# Альтернативный способ нахождения максимума
+# one_line = []
+# for row in matrix:
+#     one_line.extend(row)
+# maximum = max(one_line)
+#
+# columns = []
+# for i in range(m):
+#     for j in range(n):
+#         if matrix[i][j] == maximum:
+#             if j not in columns:
+#                 columns.append(j)
+# print('------------------')
+# for i in range(m):
+#     for col in columns:
+#         print(matrix[i][col], end='\t')
+#     print()
 
-# matrix = [[random.randint(1, 10) for i in range(5)] for j in range(5)]
 
-import random
-
-
+# 4.
+# import random
+#
+# n, m = 3, 2
+# matrix = [[random.randint(1, 20) for i in range(n)] for j in range(m)]
+#
+# for row in matrix:
+#     for num in row:
+#         print(num, end='\t')
+#     print()
+#
+# lucky_nums = []
+# for i in range(m):
+#     for j in range(n):
+#         num = matrix[i][j]
+#         minimum = num == min(matrix[i])
+#         maximum = True
+#         for k in range(m):
+#             if num < matrix[k][j]:
+#                 maximum = False
+#         if minimum and maximum:
+#             lucky_nums.append(num)
+#
+# print(lucky_nums)
