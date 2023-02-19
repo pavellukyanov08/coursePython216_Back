94601e31965a73e1f6b331c07eec581377f51452
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-19 17:37:44 +0700

    Task 1, 2, 4 from module 6

--- HomeWork/Module6/Module6.py
+++ HomeWork/Module6/Module6.py
@@ -27,14 +27,44 @@
 #
 #
 # def simp_number(number):
-#     print('Список:', number)
+#     print('Список:', number)  # выводим исходный список
 #     count = 0
-#     for i in number:
-#         number.remove(int(input('Enter the number to delete: ')))
-#         count += 1
+#     num = int(input('Enter the number to delete: '))
+#     for i in number[:-1]:   # перебираем числа списка
+#         if i == num:    # если число в списке равно числу на удаление
+#             number.remove(num)  # удаляем это число
+#             count += 1  # увеличиваем счетчик
+#
 #     print('Количество удаленных чисел:', count)
 #
 #     return number
 #
 #
 # print(simp_number([randint(1, 50) for _ in range(10)]))
+
+
+# Task 4
+# import math
+# from math import sqrt, pi
+#
+#
+# def calc_square():
+#     figure = input('Введите фигуру (1 - треуг, 2 - прямоуг, 3 -  круг): ')
+#     if figure == '1':
+#         a = float(input("a = "))
+#         b = float(input("b = "))
+#         c = float(input("c = "))
+#         p = (a + b + c) / 2
+#         return round(sqrt(p * (p - a) * (p - b) * (p - c)), 2)
+#     elif figure == '2':
+#         a = float(input("a = "))
+#         b = float(input("b = "))
+#         return a * b
+#     elif figure == '3':
+#         r = float(input('Радиус (r): '))
+#         return round(r ** 2 * math.pi, 2)
+#     else:
+#         return 'Ошибка ввода'
+#
+#
+# print(calc_square())
