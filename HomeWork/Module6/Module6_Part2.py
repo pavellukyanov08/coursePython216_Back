64e121b245a79f7da53dd95a79bd4b612bec09db
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-02-26 17:07:28 +0700

    Task 3 from module6_part 2 is completed

--- HomeWork/Module6/Module6_Part2.py
+++ HomeWork/Module6/Module6_Part2.py
@@ -34,24 +34,51 @@
 
 
 # Task 2
-def island_perimeter():
-    perimeter = 0
-    m, n = len(grid), len(grid[0])
-    for i in range(m):
-        for j in range(n):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
-    print(perimeter)
+# def island_perimeter():
+#     perimeter = 0
+#     m, n = len(grid), len(grid[0])
+#     for i in range(m):
+#         for j in range(n):
+#             if grid[i][j] == 1:
+#                 perimeter += 4
+#                 if i > 0 and grid[i-1][j] == 1:
+#                     perimeter -= 2
+#                 if j > 0 and grid[i][j-1] == 1:
+#                     perimeter -= 2
+#     print(perimeter)
+#
+#
+# grid = [[0, 0, 0, 0, 0],
+#         [0, 1, 1, 0, 0],
+#         [0, 0, 0, 0, 0],
+#         [0, 0, 0, 0, 0]]
+#
+#
+# island_perimeter()
 
 
-grid = [[0, 0, 0, 0, 0],
-        [0, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]]
-
-
-island_perimeter()
+# Task 3
+# def shift_matrix(grid, k):
+#     m, n = len(grid), len(grid[0])
+#     k = k % (m * n)
+#
+#     while k > 0:
+#         # Move the last element in each row to the first element of the next row
+#         last_elem = grid[-1][-1]
+#         for i in range(m-1, 0, -1):
+#             grid[i][-1] = grid[i-1][-1]
+#         grid[0][-1] = last_elem
+#
+#         # Move each element in the row to the right
+#         for i in range(m):
+#             last_elem = grid[i][-1]
+#             for j in range(n-1, 0, -1):
+#                 grid[i][j] = grid[i][j-1]
+#             grid[i][0] = last_elem
+#
+#         k -= 1
+#
+#     return grid
+#
+#
+# print(shift_matrix())
