f4d5ddec8657c1511406f0693404c898a1e97b45
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-03-11 18:42:26 +0700

    Edited file

--- HomeWork/Module8/Module8_Part2.py
+++ HomeWork/Module8/Module8_Part2.py
@@ -59,63 +59,86 @@
 
 
 # Task 6
-# country_city = {'Russia': 'Moscow',
-#                 'Spain': 'Madrid',
-#                 'Italy': 'Rome',
-#                 'China': 'Beijing'}
-#
-# print(country_city)
-#
-# while True:
-#     command = int(input('Select command (1 - add, 2 - remove, 3 - find county 4 - replace country, 0 - exit): '))
-#
-#     if command == 1:
-#         country = input('Enter name of country: ')
-#         city = input(f'Enter capital of {country}: ')
-#
-#         country_city[country] = city
-#         print(f'Country: {country} and city: {city} was successfully added')
-#         print(country_city)
-#
-#     elif command == 2:
-#         ctr = input('Enter name of country: ')
-#
-#         if ctr in country_city:
-#             del country_city[ctr]
-#             print(f'Country: {ctr} was successfully removed ')
-#             print(country_city)
-#         else:
-#             print('Something went wrong. ')
-#
-#     elif command == 3:
-#         find_country = input('Enter the country: ').capitalize()
-#
-#         match_country = []  # создаем список для стран
-#         for key in country_city.keys():  # итерируемся по нему
-#             if find_country in key:
-#                 match_country.append(key)  # если введенная страна есть в словаре, добавляем ее в список
-#
-#         if match_country:
-#             if len(match_country) == 1:  # если страна в списке есть, то берем и выводим ее
-#                 country_name = match_country[0]
-#                 print(f'Country found: {match_country} -> {country_city[country_name]}')
-#         else:
-#             print('Country not found')
-#
-#     elif command == 4:
-#         print(country_city)
-#
-#         old_country = input('Enter an existing country: ').capitalize()
-#         new_country = input('Enter a new country: ').capitalize()
-#
-#         if old_country in country_city.keys():
-#             value = country_city.pop(old_country)
-#             country_city[new_country] = input(f'Enter the new city of {new_country}: ').capitalize()
-#             print(country_city)
-#         else:
-#             print('Country is not found')
-#
-#     elif command == 0:
-#         break
-#
-# print('Program has been stopped')
+country_city = {'Russia': 'Moscow',
+                'Spain': 'Madrid',
+                'Italy': 'Rome',
+                'China': 'Beijing'}
+
+print(country_city)
+
+while True:
+    command = int(input('Select command (1 - add, 2 - remove, 3 - find county 4 - replace country, 0 - exit): '))
+
+    if command == 1:
+        country = input('Enter name of country: ')
+        city = input(f'Enter capital of {country}: ')
+
+        country_city[country] = city
+        print(f'Country: {country} and city: {city} was successfully added')
+        print(country_city)
+
+    elif command == 2:
+        ctr = input('Enter name of country: ')
+
+        if ctr in country_city:
+            del country_city[ctr]
+            print(f'Country: {ctr} was successfully removed ')
+            print(country_city)
+        else:
+            print('Something went wrong. ')
+
+    elif command == 3:
+        print(country_city)
+        choice = input('Enter 1, Введите 1, если хотите найти одну столицу\n'
+                       'Или 2, если хотите найти все страны и столицы '
+                       'по вашему слову: ')
+        if choice == 1:
+            country = input('Введите страну: ')
+            print(f'Столица страны {country} - {country_city[country]}')
+        else:
+            part_name = input('Введите часть названия страны: ')
+            [print(f'Страна: {country}, Столица: {capital}')
+             for country, capital in country_city.items() if
+             country.startswith(part_name.lower().capitalize())]
+    elif command == 4:
+        print(country_city)
+
+        old_country = input('Enter an existing country: ').capitalize()
+        new_country = input('Enter a new country: ').capitalize()
+
+        if old_country in country_city.keys():
+            value = country_city.pop(old_country)
+            country_city[new_country] = input(f'Enter the new city of {new_country}: ').capitalize()
+            print(country_city)
+        else:
+            print('Country is not found')
+
+
+    #     for key in country_city.keys():  # итерируемся по нему
+    #         if find_country in key:
+    #             match_country.append(key)  # если введенная страна есть в словаре, добавляем ее в список
+    #
+    #     if match_country:
+    #         if len(match_country) == 1:  # если страна в списке есть, то берем и выводим ее
+    #             country_name = match_country[0]
+    #             print(f'Country found: {match_country} -> {country_city[country_name]}')
+    #     else:
+    #         print('Country not found')
+    #
+    # elif command == 4:
+    #     print(country_city)
+    #
+    #     old_country = input('Enter an existing country: ').capitalize()
+    #     new_country = input('Enter a new country: ').capitalize()
+    #
+    #     if old_country in country_city.keys():
+    #         value = country_city.pop(old_country)
+    #         country_city[new_country] = input(f'Enter the new city of {new_country}: ').capitalize()
+    #         print(country_city)
+    #     else:
+    #         print('Country is not found')
+    #
+    # elif command == 0:
+    #     break
+
+print('Program has been stopped')
