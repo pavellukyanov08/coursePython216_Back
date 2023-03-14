abd332875de1f2e520616a1ac08df1c6e0a8d46f
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-03-14 23:14:36 +0700

    Tasks in process...

---
+++ HomeWork/Module9/decorator_function.py
@@ -0,0 +1,50 @@
+# Task 1
+# def repeat_until(f, p):
+#     def closure(x):
+#         while not p(x):
+#             x = f(x)
+#         return x
+#     return closure
+#
+#
+# def half(x):
+#     return x // 2 + 1
+#
+#
+# def is_even(x):
+#     return x < 5
+#
+#
+# increment_until_5 = repeat_until(half, is_even)
+#
+# print(increment_until_5(19))
+
+
+# Task 2
+# def counter(n):
+#     def inner():
+#         nonlocal n
+#         for i in range(n, 0, -1):
+#             print(i)
+#     return inner
+#
+#
+# c = counter(5)
+# c()
+
+
+# def make_counter(n):
+#     def counter():
+#         nonlocal n
+#         if n >= 0:
+#             n -= 1
+#             print(n)
+#     return counter
+#
+#
+# c = make_counter(5)
+# c()
+# c()
+# c()
+# c()
+# c()
