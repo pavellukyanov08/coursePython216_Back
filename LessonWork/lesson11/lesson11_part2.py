329f6cb3b43ccb19d869e8c21e7ca707797f86a9
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-03-25 14:26:47 +0700

    Edit file

--- LessonWork/lesson11/lesson11_part2.py
+++ LessonWork/lesson11/lesson11_part2.py
@@ -1,70 +1,62 @@
-# # def limit_calls(limit):
-# #     def decorator(func):
-# #         def wrapper(*args, **kwargs):
-# #             nonlocal limit
-# #             if limit > 0:
-# #                 func(*args, **kwargs)
-# #                 limit -= 1
-# #             else:
-# #                 raise ValueError('Max call limit is reached')
-# #         return wrapper
-# #     return decorator
-# #
-# #
-# # @limit_calls(2)
-# # def print_hello():
-# #     print('Hello world')
-# #
-# #
-# # print_hello()
-# # print_hello()
-# # print_hello()
+# nums = [1, 2, 3, 4]
+# for i in nums:
+#     if i == 3:
+#         print(i)
+#         break
 #
 #
-# def validate_args(*types):
-#     def decorator(func):
-#         def wrapper(*args, **kwargs):
-#             all_args = list(args)
-#             all_args.extend(list(kwargs.values()))
-#             for arg, type_of in zip(all_args, types):
-#                 if type_of != type(arg):
-#                     raise TypeError(f'Expected {type_of}, got {type(arg)} instead in {arg}')
-#             return func(*args, **kwargs)
-#         return wrapper
-#     return decorator
-#
-#
-# @validate_args(int, int)
-# def summa(a, b):
-#     return a + b
-#
-#
-# print(summa(10, 10))
-
-
-from random import randint
-#
-# nums = [randint(1, 100) for _ in range(10)]
-# print(nums)
-#
-#
-# def binary_search(nums, target):
+# def binary_search(nums, key):
 #     start, end = 0, len(nums)
 #     while start <= end:
-#         current = (end + start) // 2
-#         if nums[current] == target:
-#             return current
-#         elif nums[current] < target:
-#             start = current + 1
+#         qurrent = (end + start) // 2
+#         if nums[qurrent] == key:
+#             return nums[qurrent]
+#         elif nums[qurrent] < key:
+#             start = qurrent + 1
 #         else:
-#             end = current - 1
-#     return 'There is not number'
+#             end = qurrent - 1
+#     return 'There is no number'
+#
+# from random import randint
+#
+# nums = sorted([randint(1, 100000) for i in range(100000)])
+# print(binary_search(nums, 83258))
 #
 #
-# print(binary_search(nums, int(input('Введите число: '))))
+# Сортировка Шелла
+# Худшее время O(n^2)
+# Лучшее время O(n * log^2n)
+# При оптимальном наборе шагов(Седжвика) O(n^(4/3))
 
-# nums = [randint(1, 100) for _ in range(10)]
-# print(nums)
+# import math
+#
+#
+# def shell_sort(nums):
+#     k = int(math.log2(len(nums)))
+#     interval = 2**k - 1
+#     while interval > 0:
+#         for i in range(interval, len(nums)):
+#             key = nums[i]
+#             j = i
+#             while j >= interval and nums[j-interval] > key:
+#                 nums[j] = nums[j-interval]
+#                 j -= interval
+#             nums[j] = key
+#         k -= 1
+#         interval = 2**k - 1
+#
+#     return nums
+#
+#
+# def insert_sort(nums):
+#     for i in range(1, len(nums)):
+#         key = nums[i]
+#         j = i
+#         while j >= 1 and nums[j-1] > key:
+#             nums[j] = nums[j-1]
+#             j -= 1
+#         nums[j] = key
+#     return nums
 #
 #
 # def quick_sort(nums):
@@ -72,7 +64,6 @@ from random import randint
 #         return nums
 #     pivot = nums.pop()
 #     left, mid, right = [], [pivot], []
-#
 #     for num in nums:
 #         if num < pivot:
 #             left.append(num)
@@ -83,20 +74,35 @@ from random import randint
 #     return quick_sort(left) + mid + quick_sort(right)
 #
 #
-# print(quick_sort(nums))
-
-
-import random
-def sort_cakes(pancakes):
-    j = len(pancakes)
-    while j > 0:
-        max_r = max(pancakes[:j])
-        max_r_index = pancakes.index(max_r)
-        pancakes[max_r_index:j] = reversed(pancakes[max_r_index:j])
-        j -= 1
-        print(pancakes)
-        return pancakes[::-1]
-        a = [random.randint(1, 10) for i in range(5)]
-        print(a)
-        print()
-        print(sort_cakes(a))
+# def pancake_sort(pancakes):
+#     sorted_index = 0
+#     while pancakes != sorted(pancakes, reverse=True):
+#         unsorted_part = pancakes[sorted_index:]
+#         max_index = unsorted_part.index(max(unsorted_part))
+#
+#         top = unsorted_part[max_index:][::-1]
+#         unsorted_part = (unsorted_part[:max_index] + top)[::-1]
+#
+#         pancakes = pancakes[:sorted_index] + unsorted_part
+#         sorted_index += 1
+#     return pancakes
+#
+#
+# test = False
+#
+# if test:
+#     from random import randint
+#     from time import time
+#
+#     lst = [randint(0, 10000) for i in range(10000)]
+#     funcs = [shell_sort, insert_sort, quick_sort, pancake_sort]
+#
+#     def measure_time(sort_func, array):
+#         start = time()
+#         sort_func(array)
+#         end = time()
+#         print(f'{sort_func.__name__} time: ', end - start)
+#
+#     print('with random nums: ')
+#     for func in funcs:
+#         measure_time(func, lst.copy())
