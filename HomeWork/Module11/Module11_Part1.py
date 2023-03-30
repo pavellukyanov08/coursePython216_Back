133e2990518f8a5289236f77d8ff70ac084565c4
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-03-30 11:53:57 +0700

    part 1 is completed =(

--- HomeWork/Module11/Module11_Part1.py
+++ HomeWork/Module11/Module11_Part1.py
@@ -7,7 +7,28 @@ import re
 
 
 # Task 2
-url = ''
+# url_regex = re.compile(
+#     r'^(?:http|ftp)s?://'  # протокол
+#     r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # доменное имя
+#     r'localhost|'  # localhost
+#     r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # IP-адрес
+#     r'(?::\d+)?'  # порт
+#     r'(?:/?|[/?]\S+)$', re.IGNORECASE)  # путь
+# # Пример использования:
+# url = 'https://www.example.com/path/to/file.html?param=value'
+# if url_regex.match(url):
+#     print('Ссылка введена корректно')
+# else:
+#     print('Ссылка введена некорректно')
 
 
 # Task 3
+# text = 'Hello world! This is a Test string.'
+# matches = re.findall(r'[A-Z][a-zA-Z]*', text)
+# print(matches)
+
+
+# Task 4
+# text = 'The book is on the shelf. Letter to the editor.'
+# matches = re.findall(r'(\w)\1', text)
+# print(matches)
