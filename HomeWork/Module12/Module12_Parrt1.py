7376c8b8d0e418cd20d1d8e5f523f050f7dccc48
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-04-05 21:37:52 +0700

    task in process...

--- HomeWork/Module12/Module12_Parrt1.py
+++ HomeWork/Module12/Module12_Parrt1.py
@@ -1,49 +1,96 @@
-employees = {}
-
-
 employees_file = 'employees.txt'
+
+employees_list = []
+
+
+def loading_employees():
+    with open(file=employees_file, mode='r', encoding='utf-8') as emp_file:
+        for line in emp_file:
+            name, surname, age, post, salary = line.strip().split(',')
+            employees = [name, surname, age, post, salary]
+    print(employees)
+
+
+def saving_employees():
+    with open(file='employees_file', mode='w', encoding='utf-8') as emp_file:
+        for employee in employees_list:
+            emp_file.write(f"{employee['last_name']},"
+                           f"{employee['first_name']},"
+                           f"{employee['age']},"
+                           f"{employee['position']}"
+                           f"{employee['salary']}")
 
 
 def add_employee():
-    name = input('Enter the name of employee: ')
-    surname = input('Enter the surname of employee: ')
-    age = int(input('Enter the age of employee: '))
-    post = input('Enter the post of employee: ')
-    employees[surname] = {'name': name, 'age': age, 'post': post}
-    print(f'An employee has been added. \nCurrent list of employee: {employees}')
-# while True:
-#     command = int(input('Select command (1 - add, 2 - edit, 3 - search 4 - remove, 5 - output): '))
-#
-#     if command == 1:
-#         def add_employee():
-#             name = input('Enter the name of employee: ')
-#             surname = input('Enter the surname of employee: ')
-#             age = int(input('Enter the age of employee: '))
-#             post = input('Enter the post of employee: ')
-#
-#             employees[surname] = {'name': name, 'age': age, 'post': post}
-#             print(f'An employee has been added. \nCurrent list of employee: {employees}')
-#
-#     elif command == 2:
-#         def edit_employee():
-#             surname = input('Enter the surname of employee: ')
-#             if surname in employees:
-#                 employees[surname] = {'name': input('Enter new name'),
-#                                       'age': int(input('Enter new age')),
-#                                       'post': input('Enter new post')}
-#                 print(f'Employee details have been changed. \n{employees}')
-#             else:
-#                 print('Entered employee does not exist')
-#
-#     elif command == 3:
-#         def search_by_surname():
-#             surname = input('Enter the surname of employee: ')
-#
-#             if surname in employees:
-#                 print(f"Employee found: "
-#                       f"{employees[surname]['name']}, "
-#                       f"{employees[surname]['age']} "
-#                       f"{employees[surname]['post']}")
-#             else:
-#                 print('Employee not found')
+    name = input('Введите имя сотрудника: ')
+    surname = input('Введите фамилию сотрудника: ')
+    age = int(input('Введите возраст сотрудника: '))
+    post = input('Введите должность сотрудника: ')
+    salary = float(input('Введите зарплату сотрудника: '))
+    employees_list[surname] = {'name': name, 'age': age, 'post': post, 'salary': salary}
+    print(f'Сотрудник был добавлен. \nТекущий список сотрудников: {employees_list}')
 
+
+def edit_employee():
+    surname = input('Введите фамилию сотрудника: ')
+    if surname in employees_list:
+        employees_list[surname] = {'name': input('Введите имя сотрудника'),
+                              'age': int(input('Введите возраст сотрудника')),
+                              'post': input('Введите зарплату сотрудник'),
+                              'salary': int(input('Enter new salary'))}
+        print(f'Данные сотрудника изменены. \n{employees_list}')
+    else:
+        print('Искомый сотрудник не найден')
+
+
+def delete_employee():
+    surname = int(input('Введите фамилию сотрудника для удаления: '))
+    del employees_list[surname]
+    print(employees_list)
+
+
+def search_by_surname():
+    surname = input('Введите фамилию сотрудника: ')
+    if surname in employees_list:
+        print(f"Employee found: {employees_list[surname]['name']}, "
+              f"{employees_list[surname]['age']} "
+              f"{employees_list[surname]['post']}")
+    else:
+        print('Сотрудник не найден')
+
+
+def choice_command():
+
+    while True:
+        command = int(input('Выберите команду: '
+                            '1 - вывод списка сотрудников, '
+                            '2 - добавление, '
+                            '3 - редактирование, '
+                            '4 - удаление, '
+                            '5 - поиск по фамилии, '
+                            '6 - выход (с сохранением): '))
+
+        if command == 1:
+            loading_employees()
+
+        elif command == 2:
+            add_employee()
+
+        elif command == 3:
+            edit_employee()
+
+        elif command == 4:
+            delete_employee()
+
+        elif command == 5:
+            search_by_surname()
+
+        else:
+            saving_employees()
+            break
+
+    print('Программа завершена')
+
+
+if __name__ == '__main__':
+    choice_command()
