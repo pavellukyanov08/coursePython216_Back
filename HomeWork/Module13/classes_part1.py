cf071c51edfefb2c846f9bc5b09a215d6f1810e5
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-04-08 23:49:26 +0700

    Task in process...

---
+++ HomeWork/Module13/classes_part1.py
@@ -0,0 +1,120 @@
+# Task 1
+# class Car:
+#     def __init__(self, model, year_release, manufacturer, eng_cap, car_clr, price):
+#         self.model = model
+#         self.year_release = year_release
+#         self.manufacturer = manufacturer
+#         self.eng_cap = eng_cap
+#         self.car_clr = car_clr
+#         self.price = price
+
+    # def input_model(self, model):
+    #     self.model = model
+    #     return self.model
+    #
+    # def input_manufacturer(self, manufacturer):
+    #     self.manufacturer = manufacturer
+    #     return self.manufacturer
+
+    # def input_data(self):
+        # self.model = input('Введите модель: ')
+        # self.year_release = int(input('Введите дату релиза: '))
+        # self.manufacturer = input('Введите производителя: ')
+#         self.eng_cap = float(input('Введите объем двигателя: '))
+#         self.car_clr = input('Введите цвет машины: ')
+#         self.price = int(input('Введите цену: '))
+#
+#     def output_data(self):
+#         print(f'\nМодель: {self.model};\n год выпуска: {self.year_release};\n производитель: {self.manufacturer};\n '
+#               f'объем двигателя: {self.eng_cap};\n цвет машины: {self.car_clr};\n цена: {self.price}')
+#
+#
+# car1 = Car('', '', '', '', '', '')
+#
+# car1.input_model('M5')
+# car1.input_manufacturer('BMW')
+#
+# car1.input_data()
+# car1.output_data()
+
+
+# Task 2
+# class Book:
+#     def __init__(self, name, year_release, publisher, genre, author, price):
+#         self.name = name
+#         self.year_release = year_release
+#         self.publisher = publisher
+#         self.genre = genre
+#         self.author = author
+#         self.price = price
+
+    # доступ через метод класса
+    # def input_year_release(self, year_release):
+    #     self.year_release = year_release
+    #     return self.year_release
+
+    # доступ через метод класса
+#     def input_author(self, author):
+#         self.author = author
+#         return self.author
+#
+#     def input_data(self):
+#         self.name = input('Введите название: ')
+#         # self.year_release = int(input('Введите год выпуска: '))
+#         self.publisher = input('Введите издателя: ')
+#         self.genre = input('Введите жанр: ')
+#         # self.author = input('Введите автора: ')
+#         self.price = int(input('Введите цену: '))
+#
+#     def output_data(self):
+#         print(f'\nМодель: {self.name};\n год выпуска: {self.year_release};\n издатель: {self.publisher};\n '
+#               f'объем двигателя: {self.genre};\n цвет машины: {self.author};\n цена: {self.price}')
+#
+#
+# book1 = Book('', '', '', '', '', '')
+#
+# book1.input_year_release('1941')
+# book1.input_author('Лев Толстой')
+#
+# book1.input_data()
+# book1.output_data()
+
+
+# Task 3
+# class Stadium:
+#     def __init__(self, name, opening_date, country, city, capacity):
+#         self.name = name
+#         self.opening_date = opening_date
+#         self.country = country
+#         self.city = city
+#         self.capacity = capacity
+#
+#     # доступ через метод класса
+#     def input_name(self, name):
+#         self.name = name
+#         return self.name
+#
+#     # доступ через метод класса
+#     def input_opening_date(self, opening_date):
+#         self.opening_date = opening_date
+#         return self.opening_date
+#
+#     def input_data(self):
+#         # self.name(input('Введите название: '))
+#         # self.opening_date(input('Введите дату открытия (дд.мм.гг): '))
+#         self.country = (input('Введите страну: '))
+#         self.city = (input('Введите город: '))
+#         self.capacity = (int(input('Введите вместимость стадиона: ')))
+#
+#     def output_data(self):
+#         print(f'\nНазвание: {self.name};\n дата открытия: {self.opening_date};\n страна: {self.country};\n '
+#               f'город: {self.city};\n вместимость стадиона: {self.capacity}')
+#
+#
+# stadium1 = Stadium('', '', '', '', '')
+#
+# stadium1.input_name('Арена спорта')
+# stadium1.input_opening_date('22.05.22')
+#
+# stadium1.input_data()
+# stadium1.output_data()
