eed94e0653a0d1f72e0f8310e389b942c85a1339
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-04-16 11:14:38 +0700

    The task is redone

--- HomeWork/Module13/oop_staticmethod.py
+++ HomeWork/Module13/oop_staticmethod.py
@@ -1,83 +1,165 @@
 # Task 1
+from fractions import Fraction as Fr
+
+
 class Fraction:
-    # counter = 0
+    count = 0
 
     def __init__(self, numerator, denominator):
         self.__numerator = numerator
         self.__denominator = denominator
-        # Fraction.counter += 1
+        Fraction.count += 1
 
     @property
     def numerator(self):
         return self.__numerator
 
-    @property
-    def denominator(self):
-        return self.__denominator
-
     @numerator.setter
     def numerator(self, value):
         self.__numerator = value
+
+    @property
+    def denominator(self):
+        return self.__denominator
 
     @denominator.setter
     def denominator(self, value):
         if value == 0:
             raise ValueError('Знаменатель не может быть нулем!')
-        self.__denominator = value
-
-    def output_fraction(self):
-        print(f"{self.numerator} / {self.denominator}")
+        else:
+            self.__denominator = value
 
     def __add__(self, other):
         new_numerator = self.numerator * other.denominator + self.denominator * other.numerator
         new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+        return f'the result is {Fraction.__simplify(new_numerator, new_denominator)}'
+
+    def __add__(self, other):
+        new_numerator = self.numerator * other.denominator + self.denominator * other.numerator
+        new_denominator = self.denominator * other.denominator
+        return f'Addition: {Fraction.__simplify(new_numerator, new_denominator)}'
 
     def __sub__(self, other):
         new_numerator = self.numerator * other.denominator - self.denominator * other.numerator
         new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+        return f'Subtraction: {Fraction.__simplify(new_numerator, new_denominator)}'
 
     def __mul__(self, other):
         new_numerator = self.numerator * other.numerator
         new_denominator = self.denominator * other.denominator
-        return Fraction(new_numerator, new_denominator)
+        return f'Multiplication: {Fraction.__simplify(new_numerator, new_denominator)}'
 
     def __truediv__(self, other):
         new_numerator = self.numerator * other.denominator
         new_denominator = self.denominator * other.numerator
-        return Fraction(new_numerator, new_denominator)
+        return f'Division: {Fraction.__simplify(new_numerator, new_denominator)}'
+
+    @staticmethod
+    def __simplify(numerator, denominator):
+        return Fr(numerator, denominator)
 
     @staticmethod
     def counter():
-        print(f'counter = {Fraction.counter}')
+        print(f'counter = {Fraction.count}')
 
 
-fr1 = Fraction(10, 15)
-fr2 = Fraction(15, 15)
+f1 = Fraction(3, 7)
+f2 = Fraction(2, 9)
 
-result_addition = fr1.__add__(fr2)
-result_subtraction = fr1.__sub__(fr2)
-result_multiplication = fr1.__mul__(fr2)
-result_division = fr1.__truediv__(fr2)
+print(f1 + f2)
+print(f1 - f2)
+print(f1 * f2)
+print(f1 / f2)
 
-print("Addition:")
-result_addition.output_fraction()
-print()
+Fraction.counter()
 
-print("Subtraction:")
-result_subtraction.output_fraction()
-print()
-
-print("Multiplication:")
-result_multiplication.output_fraction()
-print()
-
-print("Division:")
-result_division.output_fraction()
-print()
-
-# Fraction.counter()
+#
+# from fractions import Fraction
+#
+#
+# class Fraction:
+#     count = 0
+#
+#     def __init__(self, numerator, denominator):
+#         self.numerator = numerator
+#         self.denominator = denominator
+#         Fraction.count += 1
+#
+#     @property
+#     def numerator(self):
+#         return self.__numerator
+#
+#     @property
+#     def denominator(self):
+#         return self.__denominator
+#
+#     @numerator.setter
+#     def numerator(self, value):
+#         self.__numerator = value
+#
+#     @denominator.setter
+#     def denominator(self, value):
+#         if value == 0:
+#             raise ValueError('Знаменатель не может быть нулем!')
+#         else:
+#             self.__denominator = value
+#
+#     def output_fraction(self):
+#         print(f"{self.numerator} / {self.denominator}")
+#
+#     def __add__(self, other):
+#         new_numerator = self.numerator * other.denominator + self.denominator * other.numerator
+#         new_denominator = self.denominator * other.denominator
+#         return Fraction(new_numerator, new_denominator)
+#
+#     def __sub__(self, other):
+#         new_numerator = self.numerator * other.denominator - self.denominator * other.numerator
+#         new_denominator = self.denominator * other.denominator
+#         return Fraction(new_numerator, new_denominator)
+#
+#     def __mul__(self, other):
+#         new_numerator = self.numerator * other.numerator
+#         new_denominator = self.denominator * other.denominator
+#         return Fraction(new_numerator, new_denominator)
+#
+#     def __truediv__(self, other):
+#         new_numerator = self.numerator * other.denominator
+#         new_denominator = self.denominator * other.numerator
+#         return Fraction(new_numerator, new_denominator)
+#
+#     # @staticmethod
+#     # def __sim
+#
+#     @staticmethod
+#     def counter():
+#         print(f'counter = {Fraction.counter}')
+#
+#
+# fr1 = Fraction(10, 15)
+# fr2 = Fraction(15, 15)
+#
+# result_addition = fr1.__add__(fr2)
+# result_subtraction = fr1.__sub__(fr2)
+# result_multiplication = fr1.__mul__(fr2)
+# result_division = fr1.__truediv__(fr2)
+#
+# print("Addition:")
+# result_addition.output_fraction()
+# print()
+#
+# print("Subtraction:")
+# result_subtraction.output_fraction()
+# print()
+#
+# print("Multiplication:")
+# result_multiplication.output_fraction()
+# print()
+#
+# print("Division:")
+# result_division.output_fraction()
+# print()
+#
+# Fraction.count()
 
 
 # Task 2
