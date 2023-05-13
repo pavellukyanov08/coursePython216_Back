5357078477fdeaf1e92250af24e605703182d65c
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-05-13 17:05:05 +0700

    lesson 18(MVC) work

---
+++ LessonWork/lesson18(MVC)/controller.py
@@ -0,0 +1,41 @@
+from model import Model, DecodeError
+from view import View
+
+
+class Controller:
+    def __init__(self):
+        self.view = View()
+        try:
+            self.model = Model()
+        except DecodeError as e:
+            self.view.__throw_an_error__(e)
+
+    def run(self):
+        query = None
+        while query != 'Выход':
+            query = self.view.wait_user_answer()
+            self.check_user_answer(query)
+
+    def check_user_answer(self, query):
+        if query == '1':
+            article_data = self.view.add_new_article()
+            self.model.add_new_article(article_data)
+
+        elif query == '2':
+            all_articles = self.model.articles
+            self.view.print_articles(all_articles)
+
+        elif query == '3':
+            criteria = self.view.find_article()
+            articles = self.model.find_articles(criteria)
+            self.view.print_articles(articles)
+
+        elif query == '4':
+            article_name = self.view.get_article()
+            articles = self.model.find_articles([article_name])
+            result = self.model.delete_article(articles)
+            if result == 'Слишком много статей':
+                self.view.delete_result(result)
+                number = self.view.get_deletion_context()
+                result = self.model.delete_article([articles[number - 1]])
+            self.view.return_delete_result(result)
