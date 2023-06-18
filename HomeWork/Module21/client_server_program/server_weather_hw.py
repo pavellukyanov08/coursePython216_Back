6eff80b18415b7eb4f8671dffadaf781656d80a6
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-06-18 12:12:10 +0700

    Task client server is completed

--- HomeWork/Module21/client_server_program/server_weather_hw.py
+++ HomeWork/Module21/client_server_program/server_weather_hw.py
@@ -17,6 +17,7 @@ server_socket.listen(5)
 
 welcome_message = 'Добро пожаловать! Вы подключены к серверу о погоде.\n'
 
+
 def handle_client(client_socket, client_address):
     print(f'Подключился клиент: {client_address}')
     client_socket.send(welcome_message.encode())
@@ -30,12 +31,15 @@ def handle_client(client_socket, client_address):
 
         def weather_forecast(q: str = 'Barnaul, RU', appid: str = api_key, units: str = 'metric'):
             response = requests.get(url_base + 'forecast', params={'q': q, 'appid': appid, 'units': units}).json()
+
             week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
             if response["cod"] == '404':
                 print("Город не найден.")
             else:
+                weather_info = ''
                 for i in range(7):
                     day = response['list'][i]
+                    date = day['dt_txt']
                     weather = day['weather'][0]['description']
                     avg_temp = round((day['main']['temp_max'] + day['main']['temp_min']) / 2)
                     temp = day['main']['temp']
@@ -43,16 +47,17 @@ def handle_client(client_socket, client_address):
                     min_temp = day['main']['temp_min']
                     humidity = day["main"]["humidity"]
                     wind_speed = day["wind"]["speed"]
-                    weather_info = (
-                        f'\nWay of the week: {week_days[i]}\n'
-                        f'Description: {weather}\n'
-                        f'Max temp: {max_temp}\n'
-                        f'Min temp: {min_temp}\n'
-                        f'Average temperature: {avg_temp}\n'
-                        f'Humidity %: {humidity}\n'
-                        f'Speed of wind (m/s): {wind_speed} \n'
-                    )
-                    client_socket.send(weather_info.encode())
+                    weather_info += f'Date: {date}\n' \
+                                    f'\nWay of the week: {week_days[i]}\n' \
+                                    f'Description: {weather}\n' \
+                                    f'Max temp: {max_temp}\n' \
+                                    f'Min temp: {min_temp}\n' \
+                                    f'Average temperature: {avg_temp}\n' \
+                                    f'Humidity %: {humidity}\n' \
+                                    f'Speed of wind (m/s): {wind_speed}\n' \
+                                    f'==========================================\n'
+                client_socket.send(weather_info.encode())
+
         weather_forecast(client_message.strip())
     client_socket.close()
 
