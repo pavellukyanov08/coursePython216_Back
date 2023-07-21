f86eb72095cb5201a0c73b76b5e5b9e47f553359
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-07-21 18:06:49 +0700

    Some changes have been made.

--- HomeWork/Module24(Flask)/main.py
+++ HomeWork/Module24(Flask)/main.py
@@ -14,6 +14,8 @@ def index():
     return render_template('index.html', title='Главная', menu=menu)
 
 
+
+
 @app.route('/about')
 def about():
     return render_template('about.html', title='Об авторе', menu=menu)
