d8ceb5263620699ca725ec5b2224fb39caa53bcf
Pavel Lukyanov <pasha.lukyanov08@gmail.com>
2023-08-26 14:06:53 +0700

    update some files

--- LessonWork/lesson28(Django)/personal_portfolio/blog/models.py
+++ LessonWork/lesson28(Django)/personal_portfolio/blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=2000)
     description = models.TextField()
-    date = models.DateField()
+    date = models.DateField(null=True)
 
     def __str__(self):
         return self.title
